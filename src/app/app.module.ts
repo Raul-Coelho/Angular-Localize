fb38c7ad45789e965c301b21060b65b3dfce12b3
Raul <raulcc50@gmail.com>
2020-08-31 15:38:35 -0300

    feat: registerLocaleData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { LocalizeService } from './services/localize.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import '@angular/common/locales/global/pt';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import '@angular/common/locales/global/pt';
     AppRoutingModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'pt' }
+    {
+      provide: LOCALE_ID,
+      deps: [LocalizeService],
+      useFactory: (localizeService) => localizeService.getLocale()
+    }
   ],
   bootstrap: [AppComponent]
 })
